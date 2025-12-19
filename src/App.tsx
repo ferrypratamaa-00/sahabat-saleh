cba165e44be901a88b45296913868c7137f359cc
ferrypratamaa-00 <ferrypratamaa-00@users.noreply.github.com>
2025-12-19 09:52:09 +0700

    remove package unused

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, memo } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import './App.css';
 import OpeningStory from './components/OpeningStory';
 import GameMenu from './components/GameMenu';
